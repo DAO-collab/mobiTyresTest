ad0b1086eb972198128e20a03eea07e07f01a870
raymond <stancillousray@gmail.com>
2024-02-24 12:05:45 +0300

    Edit: add login/register/logout in the state

--- frontend/src/stores/state.js
+++ frontend/src/stores/state.js
@@ -1,9 +1,14 @@
 import { defineStore } from 'pinia'
-
+import axiosInstance from '@/lib/axios'
 export const useAppStore = defineStore("appStore", {
     state: ()=> ({
+        authUser: null,   /**will tell if user is auth or not */
         errorToastMessage: ""   /**will hold any errors to be showed to user */
     }),
+
+    getters: {
+        user: (state) => state.authUser
+    },
 
     actions: {
         /**func to handle showing errors on UI */
@@ -32,6 +37,71 @@ export const useAppStore = defineStore("appStore", {
                 }
                 toastMessagePopup?.classList.add("showToastMessage")
             }
+        },
+
+        /**function to hande getting csrf token */
+        async getToken() { 
+            await axiosInstance.get("/sanctum/csrf-cookie")
+        },
+        async getUser() {
+            // await this.getToken()
+            try {
+                const data = await axiosInstance.get("/api/user")
+                this.authUser = data.data
+                
+            } catch (error) {
+            }
+        },
+
+        /**handle user login */
+        async loginUser (loginDetails) {
+            // await this.getToken()
+        
+            try {
+                const {data} = await axiosInstance.post('/login', {
+                    email: loginDetails.email,
+                    password: loginDetails.password,
+                });
+        
+
+                window.alert('Login successful');
+                window.location.href = "/"
+        
+            } catch (error) {
+                console.log("error ", error);
+                alert('Login failed. check your credentials and try again');
+            }
+        },
+
+        /**handle user registration */
+        async registerUser(registerDetails) {
+            // await this.getToken()
+            try {
+                const {data} = await axiosInstance.post('/register',{
+                    email: registerDetails.email,
+                    name: registerDetails.name,
+                    password: registerDetails.password
+                });
+
+                // Handle successful login (you might want to redirect or perform other actions)
+                alert('sign up successful');
+                window.location.href = "/"
+
+            } catch (error) {
+                console.log("err ", error)
+                // Handle login failure (display an error message, etc.)
+                alert('sign up failed.');
+            }
+        },
+
+        async handleLogout() {
+            try {
+                await axiosInstance.post("/logout")
+                this.authUser = null
+            } catch (error) {
+                alert("Failed to logout. Try again")
+            }
         }
+        
     }
 })
