871615379ded46a7d73da3d909e00b4da7a92d2d
raymond <stancillousray@gmail.com>
2024-02-24 12:47:12 +0300

    Delete: Remove log statements

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,18 +1,5 @@
 import axios from 'axios'
 
-/** to be used to show errors to user on ui
-import { createApp } from 'vue'
-
-import { createPinia } from 'pinia'
-import App from '@/App.vue'
-
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
-import { useAppStore } from '@/stores/state.js'
-const state = useAppStore()
-console.log("state ", state.step);
-*/
 const axiosInstance = axios.create({
     baseURL: "http://localhost:8000",
     timeout: 60000,
@@ -21,11 +8,5 @@ const axiosInstance = axios.create({
     xsrfHeaderName: "X-XSRF-TOKEN"
 })
 
-/** 
-axiosInstance.interceptors.response.use(null, (err)=> {
-    console.log("axios err ", err);
-    state.handleRequestErrors(err)
-    console.log("passed");
-})
-*/
+
 export default axiosInstance
