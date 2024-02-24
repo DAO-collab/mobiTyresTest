719c70efff14226713ee0399b9a19aa617ab9e67
raymond <stancillousray@gmail.com>
2024-02-24 10:27:42 +0300

    Add: loading screen

---
+++ frontend/src/components/LoadingScreen.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="ord-wrp">
+        <div>
+            <img src="https://c.tenor.com/-NoKc-auITEAAAAC/tenor.gif" alt="loading spinner">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.ord-wrp{
+    height: 30rem;
+    width: 100vw;
+    display: grid;
+    place-items: center;
+
+    img{
+        width: 24rem;
+        height: 24rem;
+    }
+}
+
+</style>
