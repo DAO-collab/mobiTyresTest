877b50b16a09968120f04443403b009e6acf53a1
raymond <stancillousray@gmail.com>
2024-02-24 12:46:47 +0300

    Add: code to run before each route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import axiosInstance from '@/lib/axios'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +12,8 @@ const router = createRouter({
     },
     {
       path: '/update_product/:productId',
-      component: ()=> import ('../views/Products/Update/productId.vue')
+      component: ()=> import ('../views/Products/Update/productId.vue'),
+
 
     },
     {
@@ -27,13 +29,38 @@ const router = createRouter({
     {
       path: '/add_product',
       name: 'addproduct',
-      component: ()=> import ('../views/Products/AddProduct.vue')
+      component: ()=> import ('../views/Products/AddProduct.vue'),
+
     }
   ]
 })
 
+/**will tell if user is Auth, if so, they can access
+ * certain routes
+ */
+let isAuthenticated = false
+
+/**function to check if user is authenticated */
+async function getUser() {
+  // await this.getToken()
+  try {
+      const data = await axiosInstance.get("/api/user")
+      if (data.status === 200) {
+        isAuthenticated = true
+      }
+      
+  } catch (error) {
+  }
+}
+
+
+/**to be called before each route */
+router.beforeEach(async (to, from) => {
+  await getUser()
+  if (!isAuthenticated && to.name !== 'login' && to.name !=='register') {
+    // redirect the user to the login page
+    return { name: 'login' }
+  } 
+})
+
 export default router
-
-
-// SQLSTATE[23000]: Integrity constraint violation: 1048 Column 'selling_price' cannot be null (Connection: mysql, SQL: insert into `products` (`name`, `store_location`, `selling_price`, `buying_price`, `quantity`, `status`, `updated_at`, `created_at`)
-// values (Monitor, location, ?, ?, ?, inStock, 2024-02-23 13:14:34, 2024-02-23 13:14:34))
